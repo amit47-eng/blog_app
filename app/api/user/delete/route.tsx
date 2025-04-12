b31f0f240ef0f02e1ca3e98517f314bd4969b074
amit47-eng <av495267@gmail.com>
2025-04-12 16:11:37 +0530

    error handel

--- app/api/user/delete/route.tsx
+++ app/api/user/delete/route.tsx
@@ -1,13 +1,13 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import User from "../../../../model/user.model"; 
+import User from "../../../../model/user.model";
 
-export default function DeleteUser(req: NextApiRequest, res: NextApiResponse) {
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
     let useremail = req.body.email;
     User.deleteMany({ email: useremail })
-        .then((response:any) => {
+        .then((response: any) => {
             res.status(200).json({ message: "Users deleted successfully", response: response });
         })
-        .catch((err:any) => {
+        .catch((err: any) => {
             console.error(err); // Log the error properly
             res.status(500).json({ message: "Something's wrong", error: err });
         });
